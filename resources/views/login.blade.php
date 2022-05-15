03f23e88528ae751dfb9b191eccfba5d436fc470
yazan <yazanalqadi000@hmail.com>
2022-05-15 16:47:41 +0300

    first front

---
+++ resources/views/login.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="CSS/main_login.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;500&family=Roboto+Slab&display=swap"
+            rel="stylesheet">
+        <title>Log-in</title>
+    </head>
+
+    <body>
+
+        <div class="pic"></div>
+
+        <div class="form-login-div">
+            <form class="form-login-1">
+                <div class="form-title">Sign in</div>
+                <div class="wrap-input" data-validate="Valid email is required: ex@abc.xyz">
+                    <span class="label-input">Email</span>
+                    <input class="input1" type="email" name="email" required placeholder="Email address...">
+                </div>
+                <div class="wrap-input" data-validate="Password is required">
+                    <span class="label-input">Password</span>
+                    <input class="input1" type="text" name="password" minlength="8" required
+                        placeholder="*************">
+                </div>
+                <div class="login-btn-confirm">
+                    <button type=" submit" class="login-form-btn">Sign In</button>
+                </div>
+                <div class="have-account">
+                    <span>Alreay have account ?</span>
+                    <a href="#" class="lin">Sign in</a>
+                </div>
+            </form>
+        </div>
+
+        </div>
+    </body>
+
+</html>
---
+++ resources/views/signup.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="CSS/main.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;500&family=Roboto+Slab&display=swap"
+            rel="stylesheet">
+        <title>Sign-up</title>
+    </head>
+
+    <body>
+
+        <div class="pic"></div>
+
+        <div class="form-login-div">
+            <form class="form-login-1">
+                <span class="form-login-title">Sign Up</span>
+                <div class="wrap-input" data-validate="Name is required">
+                    <span class="label-input ho">Full Name</span>
+                    <input class="input1" type="text" name="full_name" required placeholder="Name...">
+                </div>
+                <div class="wrap-input" data-validate="Valid email is required: ex@abc.xyz">
+                    <span class="label-input">Email</span>
+                    <input class="input1" type="email" name="email" required placeholder="Email address...">
+                </div>
+                <div class="wrap-input" data-validate="Username is required">
+                    <span class="label-input">Username</span>
+                    <input class="input1" type="text" name="user_name" required placeholder="Username...">
+                </div>
+                <div class="wrap-input" data-validate="Password is required">
+                    <span class="label-input">Password</span>
+                    <input class="input1" type="text" name="password" minlength="8" required
+                        placeholder="*************">
+                </div>
+                <div class="wrap-input" data-validate="Repeat Password is required">
+                    <span class="label-input">Repeat Password</span>
+                    <input class="input1" type="text" name="repeat-pass" minlength="8" required
+                        placeholder="*************">
+                </div>
+                <div class="container-login-form-btn">
+                    <div class="login-btn-confirm" style="display: inline;">
+                        <button type="submit" class="login-form-btn">Sign Up</button>
+                    </div>
+                    <div class="already-have">
+                        <span class="asq">Already have account ?</span>
+                        <a href="#" class="lin">
+                            Sign in
+                            <i class="fa fa-long-arrow-right m-l-5"></i>
+                        </a>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+        </div>
+    </body>
+
+</html>
