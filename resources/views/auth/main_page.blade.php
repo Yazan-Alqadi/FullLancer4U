9f2976182b4b1072f1255558bf79ae782408cc52
yazan <yazanalqadi000@hmail.com>
2022-05-17 17:15:48 +0300

    first front

---
+++ resources/views/auth/main_page.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/main_page_css.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;500&family=Roboto+Slab&display=swap"
+            rel="stylesheet">
+        <title>Main_Page</title>
+    </head>
+
+    <body>
+        <ul class="unor-li">
+            <li class="for-inline"><a href="#" class="main-title">Fullancer4U </a></li>
+            <li class="search-par for-inline"><input type="search" class="se-pa" placeholder="Search"></li>
+            <li class="for-inline" style="margin-left: 5%;"><a href="#" class="categories">Categories</a></li>
+            <li class="for-inline"><a href="#" class="professions">Professions</a></li>
+            <li class="for-inline"><a href="#" class="freealncers">Freealncers</a></li>
+            <li class="for-inline"><a href="#" class="projects">Projects</a></li>
+            <li class="for-inline" style="margin-left: 13%;"><a class="sign-up" href="#"> Sing up</a></li>
+            <li class="for-inline"><a href="#" class="log-in">Log in </a></li>
+        </ul>
+        <span class="continer-top-fls">
+            <a class="top-fls-title" href="#">Top Freelancers </a>
+            <div class="m-l-10">1 .........</div>
+            <div class="m-l-10">2 .........</div>
+            <div class="m-l-10">3 .........</div>
+            <div class="m-l-10">4 .........</div>
+            <div class="m-l-10">5 .........</div>
+            <div class="m-l-10">6 .........</div>
+            <div class="m-l-10">7 .........</div>
+            <div class="m-l-10">8 .........</div>
+            <div class="m-l-10">9 .........</div>
+            <div class="m-l-10">10 .........</div>
+            <div class="m-l-10">11 .........</div>
+        </span>
+
+        <span class="continer-your-fav">
+            <a class="your-fav-title" href="#">Your Favorits</a>
+            <div class="m-l-10">1 .........</div>
+            <div class="m-l-10">2 .........</div>
+            <div class="m-l-10">3 .........</div>
+            <div class="m-l-10">4 .........</div>
+            <div class="m-l-10">5 .........</div>
+            <div class="m-l-10">6 .........</div>
+            <div class="m-l-10">7 .........</div>
+            <div class="m-l-10">8 .........</div>
+            <div class="m-l-10">9 .........</div>
+            <div class="m-l-10">10 .........</div>
+            <div class="m-l-10">11 .........</div>
+        </span>
+
+
+        <div class="pro">Professions</div>
+        <div class="continer-professions">
+            <div class="pro-con"> one </div>
+            <div class="pro-con"> one </div>
+            <div class="pro-con"> one </div>
+            <div class="pro-con"> one </div>
+            <div class="pro-con"> one </div>
+            <div class="pro-con"> one </div>
+            <div class="pro-con"> one </div>
+            <div class="pro-con"> one </div>
+            <div class="pro-con"> one </div>
+            <div class="pro-con"> one </div>
+            <div class="pro-con"> one </div>
+            <div class="pro-con"> one </div>
+        </div>
+
+        <div class="pro">Projects</div>
+        <div class="continer-professions continer-projects">
+            <div class="pro-con"> one </div>
+            <div class="pro-con"> one </div>
+            <div class="pro-con"> one </div>
+            <div class="pro-con"> one </div>
+            <div class="pro-con"> one </div>
+            <div class="pro-con"> one </div>
+            <div class="pro-con"> one </div>
+            <div class="pro-con"> one </div>
+            <div class="pro-con"> one </div>
+            <div class="pro-con"> one </div>
+            <div class="pro-con"> one </div>
+            <div class="pro-con"> one </div>
+        </div>
+    </body>
+
+</html>
