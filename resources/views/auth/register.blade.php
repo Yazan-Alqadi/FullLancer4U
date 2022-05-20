895fa67b20116312d5e8b7b6133e55d593a4e731
yazan <yazanalqadi000@hmail.com>
2022-05-20 11:41:02 +0300

    first front

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,87 +1,87 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;500&family=Roboto+Slab&display=swap"
-        rel="stylesheet">
-    <title>Sign-up</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;500&family=Roboto+Slab&display=swap"
+            rel="stylesheet">
+        <title>Sign-up</title>
+    </head>
 
-<body>
+    <body>
 
-    <div class="pic"></div>
+        <div class="pic"></div>
 
-    <div class="form-login-div">
-        <form class="form-login-1" method="POST" action="{{ route('register.store') }}">
-            @csrf
-            <span class="form-login-title">Sign Up</span>
-            <div class="wrap-input" data-validate="Name is required">
-                <span class="label-input ho">Full Name</span>
-                <input class="input1" type="text" name="full_name" required placeholder="Name...">
-                @error('full_name')
+        <div class="form-login-div">
+            <form class="form-login-1" method="POST" action="{{ route('register.store') }}">
+                @csrf
+                <span class="form-login-title">Sign Up</span>
+                <div class="wrap-input" data-validate="Name is required">
+                    <span class="label-input ho">Full Name</span>
+                    <input class="input1" type="text" name="full_name" required placeholder="Name...">
+                    @error('full_name')
                     <span class="alert-error" role="alert">
-                        <strong>{{ $message }}</strong>
+                        <strong> {{ $message }} </strong>
                     </span>
-                @enderror
-            </div>
-            <div class="wrap-input" data-validate="Username is required">
-                <span class="label-input">Username</span>
-                <input class="input1" type="text" name="user_name" required placeholder="Username...">
-                @error('user_name')
-                    <span class="alert-error" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
-            <div class="wrap-input" data-validate="Valid email is required: ex@abc.xyz">
-                <span class="label-input">Email</span>
-                <input class="input1" type="email" name="email" required placeholder="Email address...">
-                @error('email')
-                    <span class="alert-error" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
-            <div class="wrap-input" data-validate="Password is required">
-                <span class="label-input">Password</span>
-                <input class="input1" type="password" name="password" minlength="8" required
-                    placeholder="*************">
-                @error('password')
-                    <span class="alert-error" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
-            <div class="wrap-input" data-validate="Repeat Password is required">
-                <span class="label-input">Repeat Password</span>
-                <input class="input1" type="password" name="password_confirmation" minlength="8" required
-                    placeholder="*************">
-            </div>
-            <div class="container-login-form-btn">
-                <div class="login-btn-confirm" style="display: inline;">
-                    <button type="submit" class="login-form-btn">Sign Up</button>
+                    @enderror
                 </div>
-                <div class="already-have">
-                    <span class="asq">Already have account ?</span>
-                    <a href="{{ route('login.show') }}" class="lin">
-                        Sign in
-                        <i class="fa fa-long-arrow-right m-l-5"></i>
-                    </a>
+                <div class="wrap-input" data-validate="Username is required">
+                    <span class="label-input">Username</span>
+                    <input class="input1" type="text" name="user_name" required placeholder="Username...">
+                    @error('user_name')
+                    <span class="alert-error" role="alert">
+                        <strong> {{ $message }} </strong>
+                    </span>
+                    @enderror
                 </div>
-            </div>
-        </form>
-    </div>
+                <div class="wrap-input" data-validate="Valid email is required: ex@abc.xyz">
+                    <span class="label-input">Email</span>
+                    <input class="input1" type="email" name="email" required placeholder="Email address...">
+                    @error('email')
+                    <span class="alert-error" role="alert">
+                        <strong> {{ $message }} </strong>
+                    </span>
+                    @enderror
+                </div>
+                <div class="wrap-input" data-validate="Password is required">
+                    <span class="label-input">Password</span>
+                    <input class="input1" type="password" name="password" minlength="8" required
+                        placeholder="*************">
+                    @error('password')
+                    <span class="alert-error" role="alert">
+                        <strong> {{ $message }} </strong>
+                    </span>
+                    @enderror
+                </div>
+                <div class="wrap-input" data-validate="Repeat Password is required">
+                    <span class="label-input">Repeat Password</span>
+                    <input class="input1" type="password" name="password_confirmation" minlength="8" required
+                        placeholder="*************">
+                </div>
+                <div class="container-login-form-btn">
+                    <div class="login-btn-confirm">
+                        <button type="submit" class="login-form-btn">Sign Up</button>
+                    </div>
+                    <div class="container-asq">
+                        <span class="asq">Already have account ?</span>
+                        <a href="{{ route('login.show') }}" class="lin">
+                            Sign in
+                            <i class="fa fa-long-arrow-right m-l-5"></i>
+                        </a>
+                    </div>
+                </div>
+            </form>
+        </div>
 
-    </div>
-</body>
+        </div>
+    </body>
 
 </html>
