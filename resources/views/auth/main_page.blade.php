c41062b7cc859cfff2dd5410f0017b073423b040
yazan <yazanalqadi000@hmail.com>
2022-06-19 20:14:09 +0300

    first front

--- resources/views/auth/main_page.blade.php
+++ resources/views/auth/main_page.blade.php
@@ -268,6 +268,61 @@
             </div>
     </section>
 
+    {{-- How works ? --}}
+    <section class="py-5 section-style" style=" background-image: url({{ 'files/bilding.jpg' }});"">
+
+        <h1 class="text-center" style="color: #560b0b !important;"> How Fullancer4U works</h1>
+
+        <div class="container text-white">
+            <div class="card bg-success mb-3">
+                <div class="card-text p-3" style="background-color: cadetblue;">
+                    <h5 class="">
+                        <span class="bg-light text-dark rounded-5 d-inline-block p-1 m-1">1</span>Browse
+                        services
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-eye-fill" viewBox="0 0 16 16">
+                            <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+                            <path
+                                d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
+                        </svg>
+                    </h5>
+                    Find the service you need using the search box at the top or via the categories.
+                </div>
+            </div>
+
+            <div class="card bg-success mb-3">
+                <div class="card-text p-3" style="background-color: darkcyan;">
+                    <h5 class=""> <span class="bg-light text-dark rounded-5 d-inline-block p-1 m-1">2</span>
+                        Request service
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-arrow-right-square-fill" viewBox="0 0 16 16">
+                            <path
+                                d="M0 14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v12zm4.5-6.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5a.5.5 0 0 1 0-1z" />
+                        </svg>
+                    </h5>
+                    Review the service description and buyers' reviews and then ask to open a connection with the
+                    seller.
+                </div>
+            </div>
+
+            <div class="card bg-success mb-3">
+                <div class="card-text p-3">
+                    <h5 class="">
+                        <span class="bg-light text-dark rounded-5 d-inline-block p-1 m-1">3</span>
+                        receive your service
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-check-square-fill" viewBox="0 0 16 16">
+                            <path
+                                d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z" />
+                        </svg>
+                    </h5>
+                    Contact the seller directly within the Fiverr website until receiving your entire order.
+                </div>
+            </div>
+        </div>
+
+    </section>
+
     <!-- Professions Cards -->
     <section class="py-5 section-style" style="background-color: #c0faff99 !important;">
 
