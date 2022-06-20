8321008e590df1c626f9ad7dd3be685a017830c1
yazan <yazanalqadi000@hmail.com>
2022-06-20 13:04:12 +0300

    first front

--- resources/views/profile_freelancer_for_client.blade.php
+++ resources/views/profile_freelancer_for_client.blade.php
@@ -13,10 +13,10 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;500&family=Roboto+Slab&display=swap"
         rel="stylesheet">
-    <link href="css/fl.css" rel="stylesheet">
+    <link href="../css/fl.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- CSS only -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
     <title>freelancer Profile</title>
 </head>
 
