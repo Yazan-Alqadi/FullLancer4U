589d4122790a836652e17081f69320a5791a45ba
yazan <yazanalqadi000@hmail.com>
2022-07-29 23:24:15 +0300

    first front

--- resources/views/auth/profile_page.blade.php
+++ resources/views/auth/profile_page.blade.php
@@ -167,8 +167,40 @@
                     </div>
                     <div class="card bg-light text-dark rounded text-center">
                         <div class="card-body">
-                            <a class="btn btn-primary" href="{{ route('become_freelancer') }}"> Add new Skill
-                            </a>
+
+                            <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas"
+                                data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">Add new skill</button>
+
+                            <div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop"
+                                aria-labelledby="offcanvasTopLabel">
+                                <div class="offcanvas-header">
+                                    <h5 class="offcanvas-title" id="offcanvasTopLabel">New skill</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas"
+                                        aria-label="Close"></button>
+                                </div>
+                                <div class="offcanvas-body">
+                                    <form action="">
+                                        <div class="input-group mb-3 w-50">
+                                            <span class="input-group-text" id="basic-addon1">skill</span>
+                                            <input type="text" class="form-control" placeholder="Type skill"
+                                                aria-label="text" aria-describedby="basic-addon1">
+                                        </div>
+                                        <div class="text-start mx-auto">
+                                            <button class="btn btn-primary" type="submit">
+                                                Add
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                    fill="currentColor" class="bi bi-plus-circle"
+                                                    viewBox="0 0 16 16">
+                                                    <path
+                                                        d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                                    <path
+                                                        d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+                                                </svg>
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
 
                         </div>
                     </div>
