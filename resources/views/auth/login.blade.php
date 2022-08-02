2dad6661ad5bbb2815ad133b6703c07d24af4e2b
yazan <yazanalqadi000@hmail.com>
2022-08-02 11:23:13 +0300

    first front

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,7 +32,7 @@
     @endif
 
 
-    <section class="container bg-light text-dark p-3 mgg w-50 w-md-50 w-sm-25 rounded">
+    <section class="container bg-light text-dark p-3 mgg mx-auto rounded" style="max-width: 25rem;">
         <div class="border-bottom border-dark ps-3 h5 py-1">
             Login
             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
@@ -41,6 +41,8 @@
                     d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zM8.5 6v1.5H10a.5.5 0 0 1 0 1H8.5V10a.5.5 0 0 1-1 0V8.5H6a.5.5 0 0 1 0-1h1.5V6a.5.5 0 0 1 1 0z" />
             </svg>
         </div>
+
+        <div class="text-center h4 p-4 border-bottom border-secondary border-opacity-25 text-secondary fw-bold">Welcome Back</div>
 
         <div>
             <form class="form-login-1 fs-6" method="POST" action="{{ route('login.store') }}">
--- resources/views/auth/profile_page.blade.php
+++ resources/views/auth/profile_page.blade.php
@@ -572,14 +572,6 @@
     <!-- JavaScript Bundle with Popper -->
     <script src=" /js/bootstrap.bundle.min.js"></script>
     <script src=" /js/popper.min.js"></script>
-    <!-- Development version -->
-    <script src="https://unpkg.com/@popperjs/core@2/dist/umd/popper.js"></script>
-
-    <!-- Production version -->
-    <script src="https://unpkg.com/@popperjs/core@2"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js"
-        integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous">
-    </script>
 
 </body>
 
