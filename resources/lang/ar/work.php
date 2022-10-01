3ae7db88975792228f0ffccee68a680a10be1bda
yazan <yazanalqadi000@hmail.com>
2022-10-01 11:05:45 +0300

    first front

---
+++ resources/lang/ar/work.php
@@ -0,0 +1,19 @@
+<?php
+
+return[
+
+    'my-work'=>'اعمالي',
+    'service'=>'الخدمة:',
+    'project'=>'المشروع:',
+    'client'=>'العميل:',
+    'state'=>'الحالة:',
+    'done'=>'انتهى',
+    'in-work'=>'قيد العمل',
+    'canceled'=>'تم الإلغاء',
+    'change-state'=>'تغيير الحالة الى:',
+    'confirm'=>'تأكيد',
+    'change-canceled'=>'إلغاء',
+    'change-done'=>'إنهاء',
+
+];
+
