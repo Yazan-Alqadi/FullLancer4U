59e70a4b5eae5cb9a1acaac535768f6c3d6e3df9
asel <asel@gmail.com>
2023-06-23 18:00:48 -0500

    add remote diagram

--- resources/views/pages/main/analysis_jobs.blade.php
+++ resources/views/pages/main/analysis_jobs.blade.php
@@ -76,6 +76,28 @@
                     voluptatibus ex nesciunt? Vero, magnam corporis.</div>
                 </divc>
             </div>
+
+
+            <div class="mt-5">
+                <div class="text-secondary h6 mb-0 p-3 text-break text-center fw-bold rounded-top bg-light"
+                    style="background-color: #e1e1e1 !important;">
+
+                    بعد إجراء دراسة حول الطلب على الخدمات والمهارات، تبيّن أن هناك طلب على العمل والخدمات المكتبيّة،
+                    سواء كان ذلك عن بُعد أو جزئيًّا. يشمل هذا الطلب بشكل عام الأعمال المتعلقة بتقنية المعلومات، التسويق
+                    الرقمي، الإدارة، التصميم، الكتابة، وغيرها من الخدمات الأخرى التي يمكن تقديمها عن بعد
+                </div>
+                <img src="{{ asset('images/diagrams/remotly onsite.png') }}" class="img-fluid" alt="...">
+                <div class="text-secondary h6 p-3 text-break text-center fw-bold rounded-bottom bg-light"
+                    style="background-color: #e1e1e1 !important;">
+
+                    تبين لنا بعد الدراسة أن النسبة الأكبر من الطلب على الخدمات والمهارات هي للعمل عن بعد، حيث يبلغ معدل
+                    هذا النوع من العمل حوالي 35٪ من إجمالي الطلب على الخدمات. يعود ذلك إلى العديد من العوامل، مثل التطور
+                    التكنولوجي وتحسين وسائل الاتصال عن بُعد، ورغبة الأفراد والشركات في تقليل التكاليف والوقت والجهد
+                    المرتبطة بالتنقل والاجتماعات الحضورية، وغيرها. كما يشير ذلك إلى أهمية توفير الخدمات الرقمية والتقنية
+                    المناسبة لتلبية هذا النوع من الطلب، وتوفير بيئة عمل مناسبة للعمل عن بُعد
+                </div>
+                </divc>
+            </div>
     </section>
 
     {{-- Footer here --}}
