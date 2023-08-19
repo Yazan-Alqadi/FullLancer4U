dbc2793978d8ce5877e0ce17240b0f3044673daf
Yazan Alqadi <yazanalqadi000@gmail.com>
2023-08-19 19:14:09 +0300

    add card

--- resources/views/layouts/nav-bar.blade.php
+++ resources/views/layouts/nav-bar.blade.php
@@ -80,11 +80,12 @@
                         </svg>
                         <span>
                             @if (Auth::user()->balance > 0)
-                                {{ Auth::user()->balance}}
+                                {{ Auth::user()->balance }}
                             @else
-                            0
+                                0
                             @endif
-                            $</span>
+                            $
+                        </span>
                     </div>
 
                     <ul class="dropdown-menu dropdown-menu-end" id="navbarDropdownMenuLink1">
@@ -324,7 +325,14 @@
                                 <path
                                     d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
                             </svg>
-                            <span>{{ Auth::user()->balance }}</span>
+                            <span>
+                                @if (Auth::user()->balance > 0)
+                                    {{ Auth::user()->balance }}
+                                @else
+                                    0
+                                @endif
+                                $
+                            </span>
                         </div>
 
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
