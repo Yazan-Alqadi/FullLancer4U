9c0966a05a409db568dd974f552c213c48a9f5bf
Basel Alhawari <basel.ah.991@gmail.com>
2023-08-21 02:58:56 +0300

    add email verfication

--- resources/views/pages/main/home_page.blade.php
+++ resources/views/pages/main/home_page.blade.php
@@ -393,7 +393,7 @@
     <div class="toast-container position-fixed bottom-0 end-0 p-3">
         <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
             <div class="toast-header">
-                <img src="..." class="rounded me-2" alt="...">
+                <img src="" class="rounded me-2" alt="">
                 <strong class="me-auto">message</strong>
                 <small>1 min ago</small>
                 <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
